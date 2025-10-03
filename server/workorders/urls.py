3ecb586333f56a6d144fe2b8d6b1d44ee8942222
jruizndev <jorsqn@gmail.com>
2025-10-03 22:58:59 +0200

    feat(backend): add DRF router for WorkOrder and Inspection endpoints

--- server/workorders/urls.py
+++ server/workorders/urls.py
@@ -1,3 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import WorkOrderViewSet, InspectionViewSet
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'workorders', WorkOrderViewSet, basename='workorder')
+router.register(r'inspections', InspectionViewSet, basename='inspection')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
