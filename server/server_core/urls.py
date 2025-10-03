5e3008e06b30e18bf9edcbe4fdf0c4bedb62a8fa
jruizndev <jorsqn@gmail.com>
2025-10-03 20:40:40 +0200

    feat(backend): configure root urls with admin, JWT auth and workorders include

--- server/server_core/urls.py
+++ server/server_core/urls.py
@@ -1,22 +1,18 @@
-"""
-URL configuration for server_core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    # Panel de administración de Django
     path('admin/', admin.site.urls),
+
+    # Endpoints de autenticación JWT
+    path('api/auth/toke/n/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Endpoints de la app workorders
+    path('api/', include('workorders.urls')),
 ]
